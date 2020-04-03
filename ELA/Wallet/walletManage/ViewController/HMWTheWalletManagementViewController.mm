835ce14af2447ccf01e7409a2bfa82892eab5a40
韩铭文 <286545761@qq.com>
2020-04-03 13:03:15 +0800

    Complete voucher function

--- ELA/Wallet/walletManage/ViewController/HMWTheWalletManagementViewController.mm
+++ ELA/Wallet/walletManage/ViewController/HMWTheWalletManagementViewController.mm
@@ -319,6 +319,7 @@ static NSString *cellString=@"HMWTheWalletManagementTableViewCell";
                 make.left.right.top.bottom.equalTo(mainView);
             }];
         }
+
     }
 }
 -(void)showDIDInfoOrCreateDIDInfo{
