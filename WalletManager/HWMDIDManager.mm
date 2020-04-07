ffe20ff1b0db0e8c294dae9c23aed751d541da4b
韩铭文 <286545761@qq.com>
2020-04-08 00:21:39 +0800

    did bug fix The English translation

--- WalletManager/HWMDIDManager.mm
+++ WalletManager/HWMDIDManager.mm
@@ -300,6 +300,7 @@ DIDAdapter *TestDIDAdapter_Create(const char *pwd, const char *walletId)
     Issuer_Destroy(isser);
     DIDURL_Destroy(creatCredentialID);
     Credential_Destroy(c);
+    NSLog(@"saveDIDCredentialWithDIDModel=====%d",r);
     if (r==0) {
         return YES;
     }else{
