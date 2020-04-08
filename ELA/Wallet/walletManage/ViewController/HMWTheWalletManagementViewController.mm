acebad2df94d049f6dc8c88f6f2641339e9b6559
韩铭文 <286545761@qq.com>
2020-04-08 13:55:17 +0800

    Merge conflicts

--- ELA/Wallet/walletManage/ViewController/HMWTheWalletManagementViewController.mm
+++ ELA/Wallet/walletManage/ViewController/HMWTheWalletManagementViewController.mm
@@ -318,7 +318,9 @@ static NSString *cellString=@"HMWTheWalletManagementTableViewCell";
             [self.toDeleteTheWalletPopV mas_makeConstraints:^(MASConstraintMaker *make) {
                 make.left.right.top.bottom.equalTo(mainView);
             }];
-      }
+
+        }
+        
     }
 }
 -(void)showDIDInfoOrCreateDIDInfo{
