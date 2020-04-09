86e8212d54ce222cc1dbca7a1b889fa159d4a7e2
韩铭文 <286545761@qq.com>
2020-04-09 17:03:31 +0800

    The English translation

--- ELA/MeVC/HWMConfidentialInformationViewController.m
+++ ELA/MeVC/HWMConfidentialInformationViewController.m
@@ -103,7 +103,6 @@ static NSString *normalCellString=@"HWMDIDListAbnormalTableViewCell";
         [self.navigationController pushViewController:WExportCertificateMVC animated:YES];
     }else{
         [[FLTools share]showErrorInfo:@"暂无"];
-        
     }
 }
 - (IBAction)TheImportEvent:(id)sender {
